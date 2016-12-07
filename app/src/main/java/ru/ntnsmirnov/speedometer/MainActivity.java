33d9cd4e8af6e6faa9dad224bfb92378cce572a5
Anton Smirnov <anton.smirnov@iac.spb.ru>
2016-12-07 12:56:33 +0300

    speed change from activity

--- app/src/main/java/ru/ntnsmirnov/speedometer/MainActivity.java
+++ app/src/main/java/ru/ntnsmirnov/speedometer/MainActivity.java
@@ -1,17 +1,52 @@
 package ru.ntnsmirnov.speedometer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Thread thread;
+    boolean stop = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Speedometer speedometer = (Speedometer)findViewById(R.id.speedometer);
-        speedometer.accelerate();
+        final Speedometer speedometer = (Speedometer) findViewById(R.id.speedometer);
+        final Random rand = new Random();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                while (!stop) {
+                    final int value = rand.nextInt(270);
+                    speedometer.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            speedometer.setSpeed(value);
+                        }
+                    });
+                    Log.d("runable", "run: newSpeed=" + value);
+                    try {
+                        Thread.sleep(900);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        thread = new Thread(runnable);
+        thread.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stop = true;
     }
 }
--- app/src/main/java/ru/ntnsmirnov/speedometer/Speedometer.java
+++ app/src/main/java/ru/ntnsmirnov/speedometer/Speedometer.java
@@ -4,11 +4,14 @@ package ru.ntnsmirnov.speedometer;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
@@ -32,7 +35,7 @@ public class Speedometer extends View {
 
     private Paint paint;
 
-    double angle = 0.75*PI;
+    double angle = 0.75 * PI;
     String speed = "0";
 
 
@@ -50,6 +53,11 @@ public class Speedometer extends View {
     float centerX = 0;
     float centerY = 0;
     float radius = 0;
+
+    Path arrow;
+    Bitmap scale;
+
+    ValueAnimator animator;
 
     public Speedometer(Context context) {
         super(context);
@@ -90,69 +98,90 @@ public class Speedometer extends View {
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeJoin(Paint.Join.ROUND);
 
-        invalidateTextPaint();
+        arrow = new Path();
     }
 
-    private void invalidateTextPaint() {
+    private void invalidateScale() {
         mTextPaint.setTextSize(mExampleDimension);
         mTextPaint.setColor(mExampleColor);
+
+        if (width > 0 && height > 0) {
+
+            scale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(scale);
+
+            canvas.drawCircle(centerX,
+                    centerY, radius, paint);
+
+            for (int num = 0; num <= 270; num += 45) {
+                double radNum = toRadians(num + 45) + PI / 2;
+                String numString = String.valueOf(num);
+
+                float cosR = (float) cos(radNum);
+                float sinR = (float) sin(radNum);
+
+                float xOnCircle = centerX + radius * cosR;
+                float yOnCircle = centerY + radius * sinR;
+
+                canvas.drawLine(
+                        xOnCircle,
+                        yOnCircle,
+                        xOnCircle - SEGMENT_LINE_OFFSET * cosR,
+                        yOnCircle - SEGMENT_LINE_OFFSET * sinR,
+                        paint);
+
+                float numWidth = mTextPaint.measureText(numString);
+                Paint.FontMetrics numFontMetrics = mTextPaint.getFontMetrics();
+                float numHeight = mTextHeight = numFontMetrics.bottom;
+
+                canvas.drawText(
+                        numString,
+                        xOnCircle - SEGMENT_NUMBER_OFFSET * cosR - numWidth / 2 - (numWidth / 2) * cosR,
+                        yOnCircle - SEGMENT_NUMBER_OFFSET * sinR + numHeight / 2 - (numHeight / 2) * sinR,
+                        mTextPaint);
+            }
+        }
+
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(centerX,
-                centerY, radius, paint);
+        if (width > 0 && height > 0) {
+            canvas.drawBitmap(scale, 0, 0, paint);
 
-        for (int num = 0; num <= 270; num += 45) {
-            double radNum = toRadians(num+45) + PI / 2;
-            String numString = String.valueOf(num);
+            mTextWidth = mTextPaint.measureText(String.valueOf(speed));
+            Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+            mTextHeight = fontMetrics.bottom;
 
-            float cosR = (float) cos(radNum);
-            float sinR = (float) sin(radNum);
-
-            float xOnCircle = centerX + radius * cosR;
-            float yOnCircle = centerY + radius * sinR;
-
-            canvas.drawLine(
-                    xOnCircle,
-                    yOnCircle,
-                    xOnCircle - SEGMENT_LINE_OFFSET * cosR,
-                    yOnCircle - SEGMENT_LINE_OFFSET * sinR,
-                    paint);
-
-            float numWidth = mTextPaint.measureText(numString);
-            Paint.FontMetrics numFontMetrics = mTextPaint.getFontMetrics();
-            float numHeight = mTextHeight = numFontMetrics.bottom;
-
-            canvas.drawText(
-                    numString,
-                    xOnCircle - SEGMENT_NUMBER_OFFSET * cosR - numWidth / 2 - (numWidth / 2) * cosR,
-                    yOnCircle - SEGMENT_NUMBER_OFFSET * sinR + numHeight / 2 - (numHeight / 2) * sinR,
+            canvas.drawText(speed,
+                    centerX  - mTextWidth / 2,
+                    centerY + 0.75f * radius - mTextHeight,
                     mTextPaint);
+
+            paint.setStyle(Paint.Style.FILL_AND_STROKE);
+            arrow.reset();
+            arrow.moveTo((float) (centerX + radius * cos(angle)),
+                    (float) (centerY + radius * sin(angle)));
+            arrow.lineTo((float) (centerX - SEGMENT_LINE_OFFSET * cos(angle - (0.10 * PI))),
+                    (float) (centerY - SEGMENT_LINE_OFFSET * sin(angle - (0.10 * PI))));
+            arrow.lineTo((float) (centerX - SEGMENT_LINE_OFFSET * cos(angle + (0.10 * PI))),
+                    (float) (centerY - SEGMENT_LINE_OFFSET * sin(angle + (0.10 * PI))));
+            arrow.lineTo((float) (centerX + radius * cos(angle)),
+                    (float) (centerY + radius * sin(angle)));
+            arrow.close();
+            canvas.drawPath(arrow, paint);
+            paint.setStyle(Paint.Style.STROKE);
         }
 
-        mTextWidth = mTextPaint.measureText(String.valueOf(speed));
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
-
-        canvas.drawText(speed,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + 0.75f*contentHeight - mTextHeight,
-                mTextPaint);
-
-        canvas.drawLine(
-                centerX,
-                centerY,
-                (float) (centerX + radius * cos(angle)),
-                (float) (centerY + radius * sin(angle)),
-                paint);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        Log.d("sdf", "onSizeChanged: ");
 
         this.paddingLeft = getPaddingLeft();
         this.paddingTop = getPaddingTop();
@@ -167,6 +196,10 @@ public class Speedometer extends View {
         centerX = paddingLeft + contentWidth / 2 + strokeWidth;
         centerY = paddingTop + contentHeight / 2 + strokeWidth;
         radius = Math.min(contentHeight, contentWidth) / 2;
+
+        invalidateScale();
+
+
     }
 
     public void accelerate() {
@@ -178,11 +211,36 @@ public class Speedometer extends View {
             public void onAnimationUpdate(ValueAnimator animation) {
                 int value = (Integer) (animation.getAnimatedValue());
                 speed = String.valueOf(value);
-                angle = toRadians(value+45) + PI / 2;
+                angle = toRadians(value + 45) + PI / 2;
                 invalidate();
             }
         });
         animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.start();
+    }
+
+    public void setSpeed(int newSpeed){
+        if (newSpeed < 0){
+            newSpeed *= -1;
+        }
+        if (newSpeed > 270) {
+            newSpeed = 270;
+        }
+        if (animator != null && animator.isRunning()){
+            animator.cancel();
+        }
+        animator = ValueAnimator.ofInt(Integer.parseInt(speed), newSpeed);
+        animator.setDuration(900);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int value = (Integer) (animation.getAnimatedValue());
+                speed = String.valueOf(value);
+                angle = toRadians(value + 45) + PI / 2;
+                invalidate();
+            }
+        });
         animator.start();
     }
 
@@ -192,7 +250,7 @@ public class Speedometer extends View {
 
     public void setExampleColor(int exampleColor) {
         mExampleColor = exampleColor;
-        invalidateTextPaint();
+        invalidateScale();
     }
 
     public float getExampleDimension() {
@@ -201,6 +259,6 @@ public class Speedometer extends View {
 
     public void setExampleDimension(float exampleDimension) {
         mExampleDimension = exampleDimension;
-        invalidateTextPaint();
+        invalidateScale();
     }
 }
